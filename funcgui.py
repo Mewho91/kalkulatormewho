9ece99a4f23bf35450b8bf549f75c2ad8f5fed50
Michał <mlewandowsky@hotmail.com>
2022-09-16 09:56:21 +0200

    wyrzucilem gui i funkcje poza main.py

--- funcgui.py
+++
@@ -1,7 +0,0 @@
-# class Funcgui:
-#     score = []
-#     def write_1(self):
-#         self.score.append(1)
-#         return self.score
-#
-#         # trzeba napisac jak ma dodawac liczby od początku do końca
