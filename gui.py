54e99ef99d468217d9259446c8e8f65afb237791
Mewho91 <92773021+Mewho91@users.noreply.github.com>
2022-10-03 18:14:23 +0200

    Add files via upload

--- gui.py
+++ gui.py
@@ -19,7 +19,7 @@ class Gui:
         root.title("Calculator")
         root.config(padx=100, pady=20)
 
-        self.main_entry = Entry(width=11, font=("Times", "45", "bold"))
+        self.main_entry = Entry(width=17, font=("Times", "30", "bold"))
         self.main_entry.grid(column=0, row=0, columnspan=4)
 
         self.przygotowanie_przyciskow_funkcyjnych()
@@ -47,42 +47,42 @@ class Gui:
         button_procent = Button(width=10, height=5, text="%", command=lambda: self.procent_pressed(1))
         button_procent.grid(column=3, row=4)
 
-        buttonR = Button(width=21, height=5, text="=", command=self.result)
+        buttonR = Button(width=21, height=5, text="=", cursor="dot", command=self.result)
         buttonR.grid(column=2, row=5, columnspan=3)
 
     def przygotowanie_przyciskow_liczbowych(self):
-
-        button1 = Button(width=10, height=5, text="1", command=lambda: self.insert_to_entry(1))
+        self.bg_color = "Grey"
+        button1 = Button(width=10, height=5, bg=self.bg_color, text="1", command=lambda: self.insert_to_entry(1))
         button1.grid(column=0, row=2)
 
-        button2 = Button(width=10, height=5, text="2", command=lambda: self.insert_to_entry(2))
+        button2 = Button(width=10, height=5, bg=self.bg_color, text="2", command=lambda: self.insert_to_entry(2))
         button2.grid(column=1, row=2)
 
-        button3 = Button(width=10, height=5, text="3", command=lambda: self.insert_to_entry(3))
+        button3 = Button(width=10, height=5, bg=self.bg_color, text="3", command=lambda: self.insert_to_entry(3))
         button3.grid(column=2, row=2)
 
-        button4 = Button(width=10, height=5, text="4", command=lambda: self.insert_to_entry(4))
+        button4 = Button(width=10, height=5, bg=self.bg_color, text="4", command=lambda: self.insert_to_entry(4))
         button4.grid(column=0, row=3)
 
-        button5 = Button(width=10, height=5, text="5", command=lambda: self.insert_to_entry(5))
+        button5 = Button(width=10, height=5, bg=self.bg_color, text="5", command=lambda: self.insert_to_entry(5))
         button5.grid(column=1, row=3)
 
-        button6 = Button(width=10, height=5, text="6", command=lambda: self.insert_to_entry(6))
+        button6 = Button(width=10, height=5, bg=self.bg_color, text="6", command=lambda: self.insert_to_entry(6))
         button6.grid(column=2, row=3)
 
-        button7 = Button(width=10, height=5, text="7", command=lambda: self.insert_to_entry(7))
+        button7 = Button(width=10, height=5, bg=self.bg_color, text="7", command=lambda: self.insert_to_entry(7))
         button7.grid(column=0, row=4)
 
-        button8 = Button(width=10, height=5, text="8", command=lambda: self.insert_to_entry(8))
+        button8 = Button(width=10, height=5, bg=self.bg_color, text="8", command=lambda: self.insert_to_entry(8))
         button8.grid(column=1, row=4)
 
-        button9 = Button(width=10, height=5, text="9", command=lambda: self.insert_to_entry(9))
+        button9 = Button(width=10, height=5, bg=self.bg_color, text="9", command=lambda: self.insert_to_entry(9))
         button9.grid(column=2, row=4)
 
-        button0 = Button(width=10, height=5, text="0", command=lambda: self.insert_zero())
+        button0 = Button(width=10, height=5, bg=self.bg_color, text="0", command=lambda: self.insert_zero())
         button0.grid(column=0, row=5)
 
-        buttonP = Button(width=10, height=5, text=".", command=lambda: self.insert_dot())
+        buttonP = Button(width=10, height=5, bg=self.bg_color, text=".", command=lambda: self.insert_dot())
         buttonP.grid(column=1, row=5)
 
     def clear_screen(self):
